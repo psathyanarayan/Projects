957a44d93c5bec4ecde116704a324365757da43d
P Sathya Narayan <60816895+psathyanarayan@users.noreply.github.com>
2020-07-20 14:20:45 +0530

    Update dice.py

--- dice/dice.py
+++ dice/dice.py
@@ -1,26 +1,57 @@
 import random
 import tkinter
+from tkinter import Label
 import IPython.display as display
-from PIL import Image
+from PIL import Image ,ImageTk
 def btnClick():
     x = random.choice([1,2,3,4,5,6])
     if x==1:
         img = Image.open("1.png")
+        img = img.resize((70,70))
+        pic = ImageTk.PhotoImage(img)
+        ig = Label(image = pic)
+        ig.image = pic
+        ig.place(x=0,y=0)
         display.display(img.resize((50,50)))
     elif x==2:
         img = Image.open("2.png")
+        img = img.resize((70,70))
+        pic = ImageTk.PhotoImage(img)
+        ig = Label(image = pic)
+        ig.image = pic
+        ig.place(x=0,y=0)
         display.display(img.resize((50,50)))
     elif x==3:
         img = Image.open("3.png")
+        img = img.resize((70,70))
+        pic = ImageTk.PhotoImage(img)
+        ig = Label(image = pic)
+        ig.image = pic
+        ig.place(x=0,y=0)
         display.display(img.resize((50,50)))
     elif x==4:
         img = Image.open("4.png")
+        img = img.resize((70,70))
+        pic = ImageTk.PhotoImage(img)
+        ig = Label(image = pic)
+        ig.image = pic
+        ig.place(x=0,y=0)
         display.display(img.resize((50,50)))
     elif x==5:
         img = Image.open("5.png")
+        img = img.resize((70,70))
+        pic = ImageTk.PhotoImage(img)
+        ig = Label(image = pic)
+        ig.image = pic
+        ig.place(x=0,y=0)
         display.display(img.resize((50,50)))
     else:
        img = Image.open("6.png")
+       img = img.resize((70,70))
+       pic = ImageTk.PhotoImage(img)
+       ig = Label(image = pic)
+       ig.image = pic
+       ig.place(x=0,y=0)
        display.display(img.resize((50,50)))    
             
 tk = tkinter.Tk()
