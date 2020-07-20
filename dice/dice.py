677baae6ce53f9e9333089dab5849f3a740d2226
P Sathya Narayan <60816895+psathyanarayan@users.noreply.github.com>
2020-07-20 14:48:01 +0530

    Update dice.py

--- dice/dice.py
+++ dice/dice.py
@@ -7,55 +7,55 @@ def btnClick():
     x = random.choice([1,2,3,4,5,6])
     if x==1:
         img = Image.open("1.png")
-        img = img.resize((70,70))
+        img = img.resize((120,120))
         pic = ImageTk.PhotoImage(img)
         ig = Label(image = pic)
         ig.image = pic
-        ig.place(x=0,y=0)
+        ig.place(x=120,y=120)
         display.display(img.resize((50,50)))
     elif x==2:
         img = Image.open("2.png")
-        img = img.resize((70,70))
+        img = img.resize((120,120))
         pic = ImageTk.PhotoImage(img)
         ig = Label(image = pic)
         ig.image = pic
-        ig.place(x=0,y=0)
+        ig.place(x=120,y=120)
         display.display(img.resize((50,50)))
     elif x==3:
         img = Image.open("3.png")
-        img = img.resize((70,70))
+        img = img.resize((120,120))
         pic = ImageTk.PhotoImage(img)
         ig = Label(image = pic)
         ig.image = pic
-        ig.place(x=0,y=0)
+        ig.place(x=120,y=120)
         display.display(img.resize((50,50)))
     elif x==4:
         img = Image.open("4.png")
-        img = img.resize((70,70))
+        img = img.resize((120,120))
         pic = ImageTk.PhotoImage(img)
         ig = Label(image = pic)
         ig.image = pic
-        ig.place(x=0,y=0)
+        ig.place(x=120,y=120)
         display.display(img.resize((50,50)))
     elif x==5:
         img = Image.open("5.png")
-        img = img.resize((70,70))
+        img = img.resize((120,120))
         pic = ImageTk.PhotoImage(img)
         ig = Label(image = pic)
         ig.image = pic
-        ig.place(x=0,y=0)
+        ig.place(x=120,y=120)
         display.display(img.resize((50,50)))
     else:
        img = Image.open("6.png")
-       img = img.resize((70,70))
+       img = img.resize((120,120))
        pic = ImageTk.PhotoImage(img)
        ig = Label(image = pic)
        ig.image = pic
-       ig.place(x=0,y=0)
+       ig.place(x=120,y=120)
        display.display(img.resize((50,50)))    
             
 tk = tkinter.Tk()
 tk.geometry("400x400")
 tk.title('Roll')
-btn = tkinter.Button(tk, text = 'Click Here To Roll', width = '20',height='10',command = btnClick).place(x="100",y="100")
+btn = tkinter.Button(tk, text = 'Roll', width = '8',height='2',command = btnClick).place(x="150",y="300")
 tk.mainloop()
